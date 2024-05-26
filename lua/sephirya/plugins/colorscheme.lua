115ade0dd9cdf0796487efd8ee2cbfa07ca9a018
Niclas Kürschner <niclas.kuerschner@outlook.com>
2024-05-26 10:37:21 +0200

    configured lsp further, changed colortheme

--- lua/sephirya/plugins/colorscheme.lua
+++ lua/sephirya/plugins/colorscheme.lua
@@ -1,8 +1,10 @@
 return {
-	"bluz71/vim-nightfly-colors",
-	lazy = false,
-	priority = 1000,
-	config = function()
-		vim.cmd [[colorscheme nightfly]]
-	end,
+  -- "bluz71/vim-nightfly-colors",
+  "folke/tokyonight.nvim",
+  lazy = false,
+  priority = 1000,
+  config = function()
+    -- vim.cmd [[colorscheme nightfly]]
+    vim.cmd [[colorscheme tokyonight-night]]
+  end,
 }
