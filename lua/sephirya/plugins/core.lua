75e980400975909f9a83b862894c4727441e7abb
Niclas Kürschner <niclas.kuerschner@outlook.com>
2024-05-26 16:15:32 +0200

    add dirtree

--- lua/sephirya/plugins/core.lua
+++ lua/sephirya/plugins/core.lua
@@ -1,24 +1,34 @@
 return {
-  "folke/which-key.nvim",
-  { "folke/neoconf.nvim", cmd = "Neoconf" },
-  "folke/neodev.nvim",
+    -- TODO configure
+    -- "folke/which-key.nvim",
+    { "folke/neoconf.nvim", cmd = "Neoconf" },
+    "folke/neodev.nvim",
 
-  "numToStr/Comment.nvim",
+    "numToStr/Comment.nvim",
 
-  { -- Adds git related signs to the gutter, as well as utilities for managing changes
-    "lewis6991/gitsigns.nvim",
-    opts = {
-      signs = {
-        add = { text = "+" },
-        change = { text = "~" },
-        delete = { text = "_" },
-        topdelete = { text = "‾" },
-        changedelete = { text = "~" },
-      },
+    { -- Adds git related signs to the gutter, as well as utilities for managing changes
+        "lewis6991/gitsigns.nvim",
+        opts = {
+            signs = {
+                add = { text = "+" },
+                change = { text = "~" },
+                delete = { text = "_" },
+                topdelete = { text = "‾" },
+                changedelete = { text = "~" },
+            },
+        },
     },
-  },
 
-  "tpope/vim-sleuth", -- Detect tabstop and shiftwidth automatically
-
-  { "folke/todo-comments.nvim", event = "VimEnter", dependencies = { "nvim-lua/plenary.nvim" }, opts = { signs = false } },
+    "tpope/vim-sleuth", -- Detect tabstop and shiftwidth automatically
+    { "folke/todo-comments.nvim", event = "VimEnter", dependencies = { "nvim-lua/plenary.nvim" }, opts = { signs = false } },
+    {
+        "nvim-tree/nvim-tree.lua",
+        dependencies = {
+            "nvim-tree/nvim-web-devicons",
+        },
+        config = function()
+            require("nvim-tree").setup({})
+            vim.keymap.set("n", "<leader>e", vim.cmd.NvimTreeToggle)
+        end,
+    },
 }
