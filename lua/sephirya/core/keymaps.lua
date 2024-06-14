4feb30383e73d6715f23ee46d0be1a44b644c361
Niclas Kürschner <niclas.kuerschner@hochbahn.de>
2024-06-14 14:59:09 +0200

    add colemak movement

--- lua/sephirya/core/keymaps.lua
+++ lua/sephirya/core/keymaps.lua
@@ -16,13 +16,6 @@ vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagn
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show diagnostic [E]rror messages' })
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
--- automatically closing brackets
-vim.keymap.set('i', '"', '""<left>')
-vim.keymap.set('i', "'", "''<left>")
-vim.keymap.set('i', '(', '()<left>')
-vim.keymap.set('i', '[', '[]<left>')
-vim.keymap.set('i', '{', '{}<left>')
-
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
 -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
 -- is not what someone will guess without a bit more experience.
@@ -31,17 +24,11 @@ vim.keymap.set('i', '{', '{}<left>')
 -- or just use <C-\><C-n> to exit terminal mode
 vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
 
--- TIP: Disable arrow keys in normal mode
-vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
-vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
-vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
-vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
-
 -- Colemak movement
--- vim.keymap.set('n', 'm', 'h')
--- vim.keymap.set('n', 'n', 'j')
--- vim.keymap.set('n', 'e', 'k')
--- vim.keymap.set('n', 'i', 'n')
+vim.keymap.set('n', 'm', 'h')
+vim.keymap.set('n', 'n', 'j')
+vim.keymap.set('n', 'e', 'k')
+vim.keymap.set('n', 'i', 'n')
 
 -- Keybinds to make split navigation easier.
 --  Use CTRL+<hjkl> to switch between windows
