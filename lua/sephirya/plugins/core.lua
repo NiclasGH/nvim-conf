d9ab7aa127bb5380ab33cf617ac46fca337b27ee
Niclas Kürschner <niclas.kuerschner@hochbahn.de>
2024-09-26 19:08:47 +0200

    add auto save plugin

--- lua/sephirya/plugins/core.lua
+++ lua/sephirya/plugins/core.lua
@@ -2,6 +2,7 @@ return {
     -- TODO configure
     -- "folke/which-key.nvim",
     { "folke/neoconf.nvim", cmd = "Neoconf" },
+
     "folke/neodev.nvim",
 
     "numToStr/Comment.nvim",
@@ -20,7 +21,9 @@ return {
     },
 
     "tpope/vim-sleuth", -- Detect tabstop and shiftwidth automatically
+
     { "folke/todo-comments.nvim", event = "VimEnter", dependencies = { "nvim-lua/plenary.nvim" }, opts = { signs = false } },
+
     {
         "nvim-tree/nvim-tree.lua",
         dependencies = {
@@ -31,4 +34,19 @@ return {
             vim.keymap.set("n", "<leader>e", vim.cmd.NvimTreeToggle)
         end,
     },
+
+    {
+        "kylechui/nvim-surround",
+        version = "*", -- Use for stability; omit to use `main` branch for the latest features
+        event = "VeryLazy",
+        trigger_events = {"InsertLeave"},
+        config = function()
+            require("nvim-surround").setup({
+                -- Use :ASToggle to toggle autosave
+                -- Configuration here, or leave empty to use defaults
+            })
+        end
+    },
+
+    "Pocco81/auto-save.nvim",
 }
