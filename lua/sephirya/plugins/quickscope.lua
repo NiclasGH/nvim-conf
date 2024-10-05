82b83f630d8242cd992b0b04a41bdb73acce3fd2
Niclas Kürschner <niclas.kuerschner@outlook.com>
2024-10-05 09:29:05 +0200

    refactor: change quickscope keys to NEST

--- lua/sephirya/plugins/quickscope.lua
+++ lua/sephirya/plugins/quickscope.lua
@@ -2,7 +2,7 @@ return {
   {
     'unblevable/quick-scope',
     init = function()
-      vim.g.qs_highlight_on_keys = {'f', 'F', 't', 'T'}
+      vim.g.qs_highlight_on_keys = {'f', 'F', 'l', 'L'}
     end
   }
 }
