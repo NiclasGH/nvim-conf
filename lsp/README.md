2ba207ca0ab76a90292260c5e835d38bd86b0d81
Niclas Kürschner <niclas.kuerschner@outlook.com>
2025-10-02 18:38:57 +0200

    feat: configure which key by adding groups

---
+++ lsp/README.md
@@ -0,0 +1,2 @@
+# LSP Folder
+Placing lua files here, exactly matching the lsp, they will be auto loaded
---
+++ lua/sephirya/plugins/which-key.lua
@@ -0,0 +1,46 @@
+return {
+  "folke/which-key.nvim",
+  event = "VeryLazy",
+  opts = {
+    preset = "helix",
+    spec = {
+      {
+        mode = { "n", "v" },
+        { "<leader>c", group = "clipboard comparison" },
+        { "<leader>f", group = "search" },
+        { "<leader>l", group = "lsp" },
+        {"<leader>x", group = "diagnostics/quickfix" },
+        { "[", group = "prev" },
+        { "]", group = "next" },
+        {
+          "<leader>b",
+          group = "buffer",
+          expand = function()
+            return require("which-key.extras").expand.buf()
+          end,
+        },
+        {
+          "<leader>w",
+          group = "windows",
+          proxy = "<c-w>",
+          expand = function()
+            return require("which-key.extras").expand.win()
+          end,
+        },
+      },
+    },
+  },
+  keys = {
+    {
+      "<leader>?",
+      function()
+        require("which-key").show({ global = false })
+      end,
+      desc = "Buffer Keymaps (which-key)",
+    },
+  },
+  config = function(_, opts)
+    local wk = require("which-key")
+    wk.setup(opts)
+  end,
+}
