a84ddf40edac7f770b17f890e4b27568afcf6d1d
Niclas Kürschner <niclas.kuerschner@outlook.com>
2025-11-02 12:37:45 +0100

    feat: add better go and rust debugging config

--- lua/tiramisu/plugins/debugging.lua
+++ lua/tiramisu/plugins/debugging.lua
@@ -1,22 +1,50 @@
 return {
-  "rcarriga/nvim-dap-ui",
+  "mfussenegger/nvim-dap",
   -- lazy = false,
   dependencies = {
-    "mfussenegger/nvim-dap",
-    "nvim-neotest/nvim-nio"
+    "leoluz/nvim-dap-go",
+    "rcarriga/nvim-dap-ui",
+    "nvim-neotest/nvim-nio",
+    "theHamsta/nvim-dap-virtual-text",
   },
   config = function()
     local dap, dapui = require("dap"), require("dapui")
-    -- breakpoint
-    vim.keymap.set("n", "<leader>db", dap.toggle_breakpoint, {})
-    -- start
-    vim.keymap.set("n", "<leader>ds", dap.continue, {})
-    -- step over (down)
-    vim.keymap.set("n", "<leader>dj", dap.step_over, {})
-    -- step into (right)
-    vim.keymap.set("n", "<leader>dl", dap.step_into, {})
-    -- inspect
-    -- vim.keymap.set("n", "<leader>di", dap.step_into(), {})
+
+    dapui.setup()
+    require("dap-go").setup()
+    -- still unsure if I like the inline values, created by virtual text
+    require("nvim-dap-virtual-text").setup()
+
+    vim.keymap.set("n", "<leader>db", dap.toggle_breakpoint, { desc = "Toggle breakpoint" })
+    vim.keymap.set("n", "<leader>ds", dap.continue, { desc = "Start/continue debugging" })
+    vim.keymap.set("n", "<leader>dt", dap.terminate, { desc = "Stop debugging" })
+    vim.keymap.set("n", "<leader>dr", dap.restart, { desc = "Restart debugger" })
+    vim.keymap.set("n", "<leader>dc", dap.run_to_cursor, { desc = "Run to cursor" })
+    vim.keymap.set("n", "<leader>dj", dap.step_over, { desc = "Step over" })
+    vim.keymap.set("n", "<leader>dl", dap.step_into, { desc = "Step into" })
+    vim.keymap.set("n", "<leader>dh", dap.step_out, { desc = "Step out" })
+    vim.keymap.set("n", "<leader>d?", function()
+      dapui.eval(nil, { enter = true })
+    end, { desc = "Inspect Variable under Cursor" })
+
+    -- barebones rust
+    dap.adapters.codelldb = {
+      type = "executable",
+      command = "codelldb",
+    }
+
+    dap.configurations.rust = {
+      {
+        name = "Launch file",
+        type = "codelldb",
+        request = "launch",
+        program = function()
+          return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+        end,
+        cwd = '${workspaceFolder}',
+        stopOnEntry = false,
+      },
+    }
 
     -- auto attach ui
     dap.listeners.before.attach.dapui_config = function()
